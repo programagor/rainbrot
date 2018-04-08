e96cca317b32cbc882b9081cc6a1f977a4993cfd
programagor <programagor@gmail.com>
2018-04-08 10:13:18 +0200

    Added features to argument passing

--- rainbrot.c
+++ rainbrot.c
@@ -1,25 +1,50 @@
+#include <stdlib.h>
 #include <stdio.h>
+#include <argp.h>
 
-#include "pointsys.h"
+#include "arguments.h"
+#include "csv_parse.h"
 
-int main(int argc,char* argv)
+int main (int argc,char** argv)
 {
-	printf("Rainbrot 0.0.1\n");
-	
-	
-	printf("Initialising iterator...\n");
-	
-	RawPtBox* masterBox=newPtBox();
-	
-	
-	
-	printf("[ok]\n");
-	
-	
-	
-	printf("Quitting...");
+struct arguments args;
+FILE *outstream;
 
-	endPtBox(masterBox);
-	
-	printf("return(0);\n");return(0);
+/* Set argument defaults */
+args.args[0] = NULL;
+int v = args.verbose = 0;
+args.re_size = 400;
+args.im_size = 400;
+args.re_min = -1;
+args.re_max =  1;
+args.im_min = -1;
+args.im_max =  1;
+args.iter = NULL;
+args.bail = 40;
+
+/* Parse command-line arguments */
+argp_parse(&argp, argc, argv, 0, 0, &args);
+
+/* Initialise unset arguments */
+if(!args.iter)
+  args.iter=csv_parse_row("10,100,1000,10000");
+for(int i=0;args.iter[i];i++)
+  {
+printf("%d\n",args.iter[i]);
+  }
+
+/* Prepare output files */
+outstream = stdout;
+  
+if(args.verbose)
+  fprintf(stdout,"Initialising array...");
+
+if(args.verbose)
+  fprintf(stdout,"[OK]\n");
+
+
+
+/* Clean up */
+free(args.iter);
+
 }
