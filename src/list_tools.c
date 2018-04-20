4c92db938042d0d2bdfcc58abd23392e3fef74ea
programagor <programagor@gmail.com>
2018-04-20 18:20:14 +0200

    Implemented input iter count sorting, duplicate detection

--- src/list_tools.c
+++ src/list_tools.c
@@ -1,7 +1,8 @@
 #include <stdint.h>
 #include <stdlib.h>
+#include <stdio.h>
 
-#include <csv_parse.h>
+#include "list_tools.h"
 
 uint64_t* csv_parse_row(char *str)
 {
@@ -22,9 +23,46 @@ uint64_t* csv_parse_row(char *str)
   /* Go through entries, fill list */
   for(i=0,j=0;str[i];i++)
     {
-      if( str[i]==',' ) j++;
+      if( str[i]==',' )
+	{
+	  if(res[j]==0) /* We don't want zero entries */
+	    {
+	      free(res);
+	      return(NULL);
+	    }
+	  else j++;
+	}
       else res[j] = res[j]*10+str[i]-'0';
     }
   return(res);
 }
 
+void sort_list(uint64_t* list)
+{
+  /* Find list length */
+  unsigned int l;
+  for(l=0;list[l];l++);
+
+  /* Replace duplicates with zeros */
+  for(unsigned int a=0;a<l-1;a++)
+    {
+      for(unsigned int b=a+1;b<l;b++)
+	{
+	  if(list[a]==list[b]) list[b]=0;
+	}
+    }
+  
+  /* Now implement Bubble Sort, with zeros last */
+  for(unsigned int a=l-1;a>0;a--)
+    {
+      for(unsigned int b=0;b<a;b++)
+	{
+	  if((list[b]>list[b+1] && list[b+1]!=0) || list[b]==0)
+	    {
+	      uint64_t swp=list[b];
+	      list[b]=list[b+1];
+	      list[b+1]=swp;
+	    }
+	}
+    }
+}
--- src/list_tools.h
+++ src/list_tools.h
@@ -1,6 +1,8 @@
-#ifndef CSV_PARSE_H
-#define CSV_PARSE_H
+#ifndef LIST_TOOLS_H
+#define LIST_TOOLS_H
 
 uint64_t* csv_parse_row(char *str);
+
+void sort_list(uint64_t* list);
 
 #endif
